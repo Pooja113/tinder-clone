3f025e2d51c5a250aae6b8eed2cffa156cc0a147
Pooja Paul <71924871+Pooja113@users.noreply.github.com>
2022-03-31 14:44:12 +0530

    Material UI error

---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./Header.css";
+import PersonIcon from '@mui/icons-material/Person';
+
+const Header = () => {
+  return (
+    <div>
+    <PersonIcon />
+      
+    </div>
+  )
+}
+
+export default Header
